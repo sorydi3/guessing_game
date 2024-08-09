03d81e09f706fd61d834ba20e0d70eb65fbcfe2e
diallo <ibrahimasory22@hotmail.com>
2024-08-09 13:36:49 +0200

    game finished

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
 
 use std::io;
 
+use rand::{thread_rng, Rng};
+
+use std::cmp::Ordering;
+
 fn main() {
     println!("GUESSING THE NUMBER GAME!!");
 
     println!("PLEASE ENTER THE NUMBER!");
 
-    let mut guess : String = String::new();
+    let secret_number:u32 = thread_rng().gen_range(1..100);
+    println!("somthing migh have been when wrong!!! SECRET NUMBER: {secret_number}");  
+    
+    loop {
 
-    let result:Result<usize ,io::Error> =io::stdin().read_line(&mut guess);
-
-    if result.is_ok() {
-        println!("you guessed the number: {guess}");
+        let mut guess : String = String::new();
+        
+        io::stdin().read_line(&mut guess).expect("I'expext the user input!!");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err (err) => {
+                println!("PARSING FILED: {err}  THE FOLLOWING DOGIT: {guess}");
+                continue;
+            }
+        };
+        
+        match guess.cmp(&secret_number){
+            Ordering::Equal =>  {println!("You win, both numbers match"); break;},
+            Ordering::Greater => println!("Your guess is higher"),  
+            Ordering::Less => println!("Your guess is lesser"),
+        }
     }
-
-    if result.is_err() {
-        println!("somthing when wrong while reading the user guess!!!")
-    }
-    //let user_guess :usize = result.expect("I don't know");
+    
+    
+    
 }
